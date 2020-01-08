864c5cb9fac8393bdcef141e79c8f381f4059e56
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2020-01-08 11:24:07 +0100

    prepare base

---
+++ js/modules/mini-react/index.js
@@ -0,0 +1,10 @@
+import { createElement, createTextElement } from './core.js';
+
+const React = {
+    createElement,
+    createTextElement,
+};
+
+console.log('React => ', React);
+
+export default React;
---
+++ js/src/index.js
@@ -0,0 +1,10 @@
+import React from '../modules/mini-react/index.js';
+
+const test = React.createElement(
+    'div',
+    { id: 'foo' },
+    React.createElement('h1', null, 'Test de create elemnt'),
+    React.createElement('span', null, 'Test de span'),
+);
+
+console.log('test ===> ', test);
