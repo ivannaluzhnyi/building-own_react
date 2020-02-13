d3d356c9440fe82d7b8a064e581f2c75a083f6b8
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2020-02-13 14:18:55 +0100

    update gitignore

--- modules/React/index.js
+++ modules/React/index.js
@@ -1,6 +1,5 @@
 import createElement from './createElement.js';
 import render, { createContext } from './render.js';
-// import  from './render.js';
 import Component from './Component.js';
 
 const React = {
